f472b0d51802ef55b7f18a9d2411438b73a4e992
manh9a111 <97293757+manh9a111@users.noreply.github.com>
2022-04-28 21:55:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Diệu Linh cute <3",
     introTitle: 'Diệu Linh',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Sau lần đầu tiên gặp Linh tớ ngỡ như ánh mắt của cậu đã đánh cắp mất trái tim của tớ rồi nghe có vẻ sến súa nhưng đó là thật `,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng Linh đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng Linh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    title: ' Nói sao nhở , một cô gái dễ thương mà đối với tớ là cả một bầu trời vậy 🥰',
+    desc: 'Tớ muốn nói với Linh một câu,mà từ rất lâu tớ chưa dám nói ra, nhưng hôm nay không nói, thì nhất định sau này không nói được.”tớ yêu Linh rất nhiều!à tớ hỏi vui Linh là giờ tớ tỏ tình thì Linh có đồng ý khum?',
+    btnYes: 'Tất nhiên là có chứ <33',
     btnNo: 'Không nha :3',
-    question: 'Tớ muốn nói với Linh một câu,mà từ rất lâu tớ chưa dám nói ra, nhưng hôm nay không nói, thì nhất định sau này không nói được.”tớ yêu Linh rất nhiều”',
+    question: 'Trước tớ có tỏ tình Linh nhưng lúc đó tớ cảm thấy nó vội vàng và chưa thực sự đủ để có được lời đồng ý từ Linh , nhưng lúc này tớ nghĩ giờ là thời điểm thích hợp để mong nhận được lời đồng ý từ Linh … 😄 Linh có muốn hẹn hò với tớ khummm !!?',
     btnReply: 'Gửi cho Mạnh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <3333333',
-    mess: 'Mình biết mà 🥰. Yêu Linh nhiều nhiều 😘😘',
+    reply: 'Làm gì có lí do để từ chối chứ <3333333',
+    mess: 'biết mà 🥰. Yêu Linh nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h,Mạnh đón Linh đi chơi nha.!!<3',
     btnAccept: 'Okiii nhá <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100009188955128' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/dieulinh1012vp' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
