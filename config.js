53b597c3a15bcac24df3537cede21d88b304e773
Mạnh Dê <97293757+manh9a111@users.noreply.github.com>
2024-06-11 13:23:26 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 const CONFIG = {
     titleWeb: "Diệu Linh cute <3",
     introTitle: 'Diệu Linh',
-    introDesc: `Sau lần đầu tiên gặp Linh tớ ngỡ như ánh mắt của cậu đã đánh cắp mất trái tim của tớ rồi nghe có vẻ sến súa nhưng đó là thật `,
+    introDesc: `ĐỒ MẮT HÍP`,
     btnIntro: '^^HiHi^^',
-    title: ' Nói sao nhở , một cô gái dễ thương mà đối với tớ là cả một bầu trời vậy 🥰',
-    desc: 'Tớ muốn nói với Linh một câu,mà từ rất lâu tớ chưa dám nói ra, nhưng hôm nay không nói, thì nhất định sau này không nói được.”tớ yêu Linh rất nhiều!à tớ hỏi vui Linh là giờ tớ tỏ tình thì Linh có đồng ý khum?',
-    btnYes: 'Tất nhiên là có chứ <33',
+    title: 'yêu anh nha!!!',
+    desc: 'yêu anh thì anh không trêu mắt híp nữa',
+    btnYes: 'Tất nhiên là đồng ý rồiiii <33',
     btnNo: 'Không nha :3',
-    question: 'Trước tớ có tỏ tình Linh nhưng lúc đó tớ cảm thấy nó vội vàng và chưa thực sự đủ để có được lời đồng ý từ Linh , nhưng lúc này tớ nghĩ giờ là thời điểm thích hợp để mong nhận được lời đồng ý từ Linh … 😄 Linh có muốn hẹn hò với tớ khummm !!?',
-    btnReply: 'Gửi cho Mạnh <3',
-    reply: 'Làm gì có lí do để từ chối chứ <3333333',
-    mess: 'biết mà 🥰. Yêu Linh nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, Mạnh đón Linh đi chơi nha.!!<3',
+    question: 'Có nhớ anh không?',
+    btnReply: 'Gửi',
+    reply: 'có rất nhớ ',
+    mess: 'anh biết',
+    messDesc: 'Tối CN anh đưa đi chơi',
     btnAccept: 'Okiii nhá <3',
-    messLink: 'https://www.youtube.com/watch?v=NlrYe__kDvE' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://manh9a111.github.io/xemphimclub-app/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
